fbe7f9517b3f01c2dae1a0591470921617f87be7
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-26 09:44:44 -0400

    added comments.

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,5 +1,7 @@
-const planets = []
+/* ---- Planets Model ---- */
+const planets = require('../../models/planets.model')
 
+/* ---- Controllers ---- */
 function getAllPlanets(req, res) {
   return res.status(200).json(planets)
 }
