001e30e5a42ec599c2ddd5ee5791283cf0b530e8
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-27 13:39:56 -0400

    added static path to serve public folder and route.

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 
@@ -10,8 +11,12 @@ const app = express()
 /* ---- Middleware ---- */
 app.use(cors({ origin: 'http://localhost:3000' })) // cors
 app.use(express.json()) // parse any JSON coming from incoming requests
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 /* ---- Routes ---- */
 app.use('/api', planetsRouter)
+app.get('/', (req, res) => {
+  res.send(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
