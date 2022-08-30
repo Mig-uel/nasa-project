e9f224c96890bc2d7af868ff6b329940e3571d40
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-30 13:49:19 -0400

    removed direct access to planets and instead export a function returning planets.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -41,7 +41,12 @@ function loadPlanetsData() {
   })
 }
 
+function getAllPlanets() {
+  // returns planets to be consumed by controller
+  return habitablePlanets
+}
+
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets,
+  getAllPlanets,
 }
