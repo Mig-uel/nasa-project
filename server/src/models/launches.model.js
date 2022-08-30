ec97e8e0cf6371629ae6bcb7cb18b6da31be4a94
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-30 15:50:13 -0400

    added addNewLaunch function that handles creation of launches.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,21 +1,39 @@
 const launches = new Map()
 
+let latestFlightNumber = 100
+
+/* ---- Launch Model ---- */
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
   destination: 'Kepler-442 b',
-  customer: ['NASA', 'ZTM'],
+  customer: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 }
 
 launches.set(launch.flightNumber, launch)
 
+/* ---- Get All Launches ---- */
 function getAllLaunches() {
   // convert data to array for controller consumption
   return Array.from(launches.values())
 }
 
-module.exports = { getAllLaunches }
+/* ---- Add New Launch ---- */
+function addNewLaunch(launch) {
+  latestFlightNumber++
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customer: ['ZTM', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  )
+}
+
+module.exports = { getAllLaunches, addNewLaunch }
