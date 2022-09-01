8b55027f6c494c0a6ade9338ceb07874ae65ad5d
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-01 18:50:11 -0400

    created abort/delete controller function.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,11 +1,17 @@
 /* ---- Models ---- */
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model')
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require('../../models/launches.model')
 
 /* ---- Get All Launches ---- */
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches())
 }
 
+/* ---- Post New Launch ---- */
 function httpAddNewLaunch(req, res) {
   const launch = req.body
 
@@ -24,4 +30,16 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch)
 }
 
-module.exports = { httpGetAllLaunches, httpAddNewLaunch }
+/* ---- Delete a Launch ---- */
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id)
+
+  // if launch does not exist
+  if (!existsLaunchWithId(launchId))
+    return res.status(404).json({ error: 'Launch not found!' })
+
+  const aborted = abortLaunchById(launchId)
+  return res.status(200).json(aborted)
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch }
