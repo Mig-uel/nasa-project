05794c69f18c230a400464a374ceb97cb0f092f6
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-10-08 17:27:23 -0400

    implemented the planets model and removed old code.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-
 const { parse } = require('csv-parse')
 
-const habitablePlanets = []
+const planets = require('./planets.mongo')
 
 function isHabitablePlanet(planet) {
   return (
@@ -25,25 +24,40 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on('data', (data) => {
+      .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data)
+          savePlanet(data)
         }
       })
       .on('error', (err) => {
         console.log(err)
         reject(err)
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`)
+      .on('end', async () => {
+        const countPlanetsFound = (await getAllPlanets()).length
+        console.log(`${countPlanetsFound} habitable planets found!`)
         resolve()
       })
   })
 }
 
-function getAllPlanets() {
+async function getAllPlanets() {
   // returns planets to be consumed by controller
-  return habitablePlanets
+  return await planets.find({})
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      { keplerName: planet.kepler_name },
+      { upsert: true }
+    )
+  } catch (error) {
+    console.error(`Could not save planet: ${error}`)
+  }
 }
 
 module.exports = {
